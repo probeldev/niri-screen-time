5142d84a72bc954861dae1b0271e499cb416b7b5
Sergey Protko <sergey.protko@yandex.ru>
2025-05-21 12:24:51 +0300

    add date flags

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"time"
 
@@ -18,6 +19,8 @@ func main() {
 	defer db.Close()
 
 	isDaemon := flag.Bool("daemon", false, "Run daemon")
+	fromStr := flag.String("from", "", "Начальная дата (формат: 2006-01-02), по умолчанию — начало сегодняшнего дня")
+	toStr := flag.String("to", "", "Конечная дата (формат: 2006-01-02), по умолчанию — конец сегодняшнего дня")
 
 	flag.Parse()
 
@@ -26,12 +29,47 @@ func main() {
 		daemon.Run(db)
 	}
 
-	from := time.Now().Add(-1 * 24 * 365 * time.Hour)
-	to := time.Now()
+	flag.Parse()
+
+	// Парсим даты (если не указаны — берём сегодняшний день)
+	from, to := parseDates(*fromStr, *toStr)
+
+	fmt.Printf("From %s to %s\n", from.Format("2006-01-02 15:04:05"), to.Format("2006-01-02 15:04:05"))
 
 	err = report.GetReport(db, from, to)
 
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func parseDates(fromStr, toStr string) (from, to time.Time) {
+	now := time.Now()
+
+	todayStart := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	todayEnd := todayStart.Add(24*time.Hour - 1*time.Nanosecond)
+
+	if fromStr == "" {
+		from = todayStart
+	} else {
+		parsedFrom, err := time.Parse("2006-01-02", fromStr)
+		if err != nil {
+			log.Panic("Error parse date")
+		} else {
+			from = parsedFrom
+		}
+	}
+
+	if toStr == "" {
+		to = todayEnd
+	} else {
+		parsedTo, err := time.Parse("2006-01-02", toStr)
+		if err != nil {
+			log.Panic("Error parse date")
+		} else {
+			to = parsedTo
+		}
+	}
+
+	return from, to
 }
