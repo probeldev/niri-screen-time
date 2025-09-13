2a5e21916cc803b83478983d8239cefb1f718070
Sergey Protko <sergey.protko@yandex.ru>
2025-09-14 00:43:28 +0300

    change format config

--- model/alias.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Alias struct {
-	Name  string `json:"name"`
-	Alias string `json:"alias"`
-}
--- report/report.go
+++ report/report.go
@@ -2,10 +2,8 @@
 package report
 
 import (
-	"log"
 	"time"
 
-	"github.com/probeldev/niri-screen-time/aliasmanager"
 	"github.com/probeldev/niri-screen-time/db"
 	"github.com/probeldev/niri-screen-time/model"
 	"github.com/probeldev/niri-screen-time/response"
@@ -69,30 +67,7 @@ func GetReport(
 		responseSlice = append(responseSlice, responseApp)
 	}
 
-	responseSlice, err = useAliace(responseSlice)
-	if err != nil {
-		return err
-	}
-
 	response.Write(responseSlice, limit)
 
 	return nil
-}
-
-func useAliace(reports []model.Report) ([]model.Report, error) {
-	fn := "useAliace"
-
-	alias, err := aliasmanager.NewAliasManager()
-	if err != nil {
-		log.Println(fn, err)
-		return nil, err
-	}
-
-	reportsWithAlias := []model.Report{}
-	for _, r := range reports {
-		alias := alias.ReplaceAppId2Alias(r)
-		reportsWithAlias = append(reportsWithAlias, alias)
-	}
-
-	return reportsWithAlias, nil
 }
